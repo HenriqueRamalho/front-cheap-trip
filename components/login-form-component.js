d547354e1972eb3705c6a89489c96bc1748b9692
tmendes-ciandt <tmendes@ciandt.com>
2022-03-19 16:26:38 -0300

    Create login page

---
+++ components/login-form-component.js
@@ -0,0 +1,45 @@
+import Link from 'next/link';
+import styles from '../styles/components/Login.module.css';
+import forms from '../styles/components/Form.module.css';
+import Image from 'next/image';
+
+
+function LoginForm() {
+	return(
+		<>
+			<h2 className={styles.login_title}> Entrar </h2>
+			<form>
+					<input type={'text'} className={forms.input_form} placeholder={'Usuário'} />
+					<input type={'password'} className={forms.input_form} placeholder={'Senha'} />
+					<button className={forms.button}> Entrar </button>
+			</form>
+
+			<div className={styles.login_box}>
+				<Link href="/#">
+					Esqueci minha senha
+				</Link>
+			</div>
+
+			<div className={styles.login_box}>
+				<button className={forms['button'] + " " + forms['button_social']}>
+					<Image
+						src={"/images/google.svg"}
+						alt='Google'
+						width={25}
+						height={24}
+						className={forms.google_img}
+						/> <span className={forms.btn_label}>Google</span>
+				</button>
+			</div>
+
+			<div className={styles.login_box}>
+				Ainda não é cadastrado? <br />
+				<Link href="/#">
+					Cadastre-se agora
+				</Link>
+			</div>
+		</>
+	);
+}
+
+export default LoginForm;
