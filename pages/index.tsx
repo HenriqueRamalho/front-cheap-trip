b680f94b45ae99eb0f4d2a7433a2cdebb8048a07
tmendes-ciandt <tmendes@ciandt.com>
2022-03-19 16:33:22 -0300

    Add head to project

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-
+import HeadProject from "../components/Head";
 import Header from '../components/header-component';
 import LoginForm from '../components/login-form-component';
 
@@ -10,10 +9,11 @@ import LoginForm from '../components/login-form-component';
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
+			<HeadProject />
       <Header />
       <LoginForm />
     </div>
-  )   
+  )
 }
 
 export default Home
