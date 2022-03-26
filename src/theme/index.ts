c21121e00abe59348ae984d6ed3cef817fcd7da8
Henrique Ramalho <henrique.antunes@hotmart.com>
2022-03-26 18:59:07 -0300

    chore: configs for material ui 4

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,37 +1,16 @@
-import { ThemeOptions, responsiveFontSizes } from '@material-ui/core/styles'
-import { createTheme } from '@material-ui/core/styles'
+import {
+	createTheme,
+	responsiveFontSizes,
+	ThemeOptions
+} from '@mui/material/styles'
 
 const theme: ThemeOptions = {
 	palette: {
 		primary: {
-			main: '#000000'
+			main: '#006BA8'
 		},
 		secondary: {
-			main: '#ff0000'
-		}
-	},
-	breakpoints: {
-		values: {
-			xs: 0,
-			sm: 600,
-			md: 960,
-			lg: 1170,
-			xl: 1920
-		}
-	},
-	overrides: {
-		MuiCssBaseline: {
-			'@global': {
-				body: {
-					backgroundColor: '#fff'
-				},
-				'b, strong': {
-					fontWeight: 500
-				},
-				a: {
-					color: '#e03936'
-				}
-			}
+			main: '#FFBC48'
 		}
 	}
 }
