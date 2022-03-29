cb0cd31476128a20901af6df8b4907c9526bb861
Nayara <dsantos.nay@gmail.com>
2022-03-28 22:58:03 -0300

    feat: add new state to contactSalles page

--- src/pages/contactSalles.tsx
+++ src/pages/contactSalles.tsx
@@ -6,8 +6,11 @@ import Grid from '@mui/material/Grid'
 import Stack from '@mui/material/Stack'
 import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
+import Image from 'next/image'
 
 const ContactSalles: NextPage = () => {
+	const [isMessengeSent, setIsMessengeSent] = React.useState(false)
+
 	return (
 		<div
 			style={{
@@ -15,66 +18,107 @@ const ContactSalles: NextPage = () => {
 				margin: '0 auto'
 			}}
 		>
-			<Box>
-				<Grid container>
-					<Grid item xs={12} pb={4}>
-						<Typography variant="h2" align="center">
-							Solicitar contato do vendedor
-						</Typography>
-					</Grid>
-				</Grid>
-			</Box>
-
-			<Box component="form" noValidate autoComplete="off">
-				<Grid container direction="column" spacing={2}>
-					<Grid item xs={12}>
-						<TextField
-							id="outlined-basic"
-							label="Nome completo"
-							variant="outlined"
-							fullWidth
-						/>
-					</Grid>
-
-					<Grid item xs={12}>
-						<TextField
-							id="outlined-basic"
-							label="E-mail"
-							variant="outlined"
-							fullWidth
-							type="email"
-						/>
+			{isMessengeSent ? (
+				<div>
+					<Grid container>
+						<Grid
+							item
+							xs={12}
+							pb={4}
+							direction={'column'}
+							sx={{
+								alignItems: 'center',
+								display: 'flex'
+							}}
+						>
+							<Image
+								src={'/images/messengeSent.svg'}
+								alt="Messenge Sent"
+								width={114}
+								height={114}
+							/>
+						</Grid>
 					</Grid>
 
-					<Grid item xs={12}>
-						<TextField
-							id="outlined-basic"
-							label="Celular"
-							variant="outlined"
-							fullWidth
-							type="tel"
-						/>
+					<Grid container>
+						<Grid item xs={12} pb={4}>
+							<Typography variant="h2" align="center">
+								Mensagem enviada com sucesso!
+							</Typography>
+						</Grid>
 					</Grid>
-					<Grid item xs={12}>
-						<TextField
-							id="outlined-basic"
-							label="Mensagem"
-							variant="outlined"
-							fullWidth
-							multiline
-							rows={3}
-						/>
-					</Grid>
+				</div>
+			) : (
+				<>
+					<Box>
+						<Grid container>
+							<Grid item xs={12} pb={4}>
+								<Typography variant="h2" align="center">
+									Solicitar contato do vendedor
+								</Typography>
+							</Grid>
+						</Grid>
+					</Box>
 
-					<Grid item xs={12}>
-						<Stack spacing={2} direction="row">
-							<Button fullWidth size={'large'} variant="contained">
-								Enviar
-							</Button>
-						</Stack>
-					</Grid>
-				</Grid>
-			</Box>
+					<Box component="form" noValidate autoComplete="off">
+						<Grid container direction="column" spacing={2}>
+							<Grid item xs={12}>
+								<TextField
+									id="outlined-basic"
+									label="Nome completo"
+									variant="outlined"
+									fullWidth
+								/>
+							</Grid>
+
+							<Grid item xs={12}>
+								<TextField
+									id="outlined-basic"
+									label="E-mail"
+									variant="outlined"
+									fullWidth
+									type="email"
+								/>
+							</Grid>
+
+							<Grid item xs={12}>
+								<TextField
+									id="outlined-basic"
+									label="Celular"
+									variant="outlined"
+									fullWidth
+									type="tel"
+								/>
+							</Grid>
+							<Grid item xs={12}>
+								<TextField
+									id="outlined-basic"
+									label="Mensagem"
+									variant="outlined"
+									fullWidth
+									multiline
+									rows={3}
+								/>
+							</Grid>
+
+							<Grid item xs={12}>
+								<Stack spacing={2} direction="row">
+									<Button
+										fullWidth
+										size={'large'}
+										variant="contained"
+										onClick={() => {
+											setIsMessengeSent(true)
+										}}
+									>
+										Enviar
+									</Button>
+								</Stack>
+							</Grid>
+						</Grid>
+					</Box>
+				</>
+			)}
 		</div>
 	)
 }
