387137ecd174cfd386d7bd6c5b2ff2f9d82a17a4
Nayara <dsantos.nay@gmail.com>
2022-03-28 21:26:57 -0300

    changes page my Account

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,8 +1,10 @@
+import { Button } from '@mui/material'
 import {
 	createTheme,
 	responsiveFontSizes,
 	ThemeOptions
 } from '@mui/material/styles'
+import { fontSize } from '@mui/system'
 
 const theme: ThemeOptions = {
 	palette: {
@@ -11,6 +13,20 @@ const theme: ThemeOptions = {
 		},
 		secondary: {
 			main: '#FFBC48'
+		}
+	},
+	typography: {
+		fontFamily: "'Nunito', 'sans-serif'",
+
+		h2: {
+			color: '#006BA8',
+			fontWeight: 900,
+			fontSize: '2rem'
+		},
+
+		button: {
+			textTransform: 'none',
+			fontWeight: 700
 		}
 	}
 }
