c7c8b8776e396b3a5571f04a4a0600d063dd17ea
Henrique Ramalho <henrique.antunes@hotmart.com>
2022-04-07 22:45:59 -0300

    feat: add components to dashboard

--- src/components/DiscoveryDestinations/index.tsx
+++ src/components/DiscoveryDestinations/index.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react'
-import CardPack from 'components/CardPack'
 import Slider from 'react-slick'
 import { useTheme } from '@material-ui/core/styles'
 import useMediaQuery from '@material-ui/core/useMediaQuery'
+import CardDestination from 'components/CardDestination'
+import { Box, Button, Typography } from '@mui/material'
 
-const CarouselWithCards = () => {
+const DiscoveryDestinations = () => {
 	const theme = useTheme()
 	const isMoreThenMd = useMediaQuery(theme.breakpoints.up('md'))
 	const settings = {
@@ -12,7 +13,7 @@ const CarouselWithCards = () => {
 		centerMode: !isMoreThenMd,
 		infinite: true,
 		centerPadding: '60px',
-		slidesToShow: isMoreThenMd ? 3 : 1,
+		slidesToShow: isMoreThenMd ? 5 : 1,
 		speed: 500
 	}
 
@@ -24,15 +25,32 @@ const CarouselWithCards = () => {
 				margin: '0 auto'
 			}}
 		>
+			<Box sx={{ paddingBottom: 4, paddingTop: 4 }}>
+				<Typography variant="h2" align="center">
+					Pacotes de viagem em destaque
+				</Typography>
+			</Box>
 			<Slider {...settings}>
 				{[1, 2, 3, 4, 5, 6].map((item) => (
 					<div key={item} style={{ width: 300, padding: '0 0' }}>
-						<CardPack />
+						<CardDestination />
 					</div>
 				))}
 			</Slider>
+			<Box
+				sx={{
+					display: 'flex',
+					justifyContent: 'center',
+					paddingBottom: 3,
+					paddingTop: 3
+				}}
+			>
+				<Button size={'large'} variant="contained" onClick={() => null}>
+					Ver todos os destinos
+				</Button>
+			</Box>
 		</div>
 	)
 }
 
-export default CarouselWithCards
+export default DiscoveryDestinations
