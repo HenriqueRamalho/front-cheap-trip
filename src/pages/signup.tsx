d9919821e4f27ec1201685efc5cc27739aa02e4d
Henrique Ramalho <henrique.antunes@hotmart.com>
2022-05-21 17:01:01 -0300

    feat: add box around signup form

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -2,12 +2,23 @@ import type { NextPage } from 'next'
 import styles from '../styles/Home.module.css'
 import Header from '../components/header-component'
 import CadastroForm from '../components/component-cadastro-form'
+import { Box } from '@mui/material'
 
 const Signup: NextPage = () => {
 	return (
 		<div className={styles.container}>
 			<Header />
-			<CadastroForm />
+			<Box
+				sx={{
+					py: 4,
+					px: 2,
+					maxWidth: 768,
+					width: '100%',
+					margin: '0 auto'
+				}}
+			>
+				<CadastroForm />
+			</Box>
 		</div>
 	)
 }
